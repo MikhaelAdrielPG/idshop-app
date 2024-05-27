22ed988c03638173c4173cda932eb48145897ac3
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-05-27 23:05:02 +0700

    Membuat Navbar Component

--- Chapter 7/vue-cli/src/App.vue
+++ Chapter 7/vue-cli/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app" class="container">
     <h1>IDShop</h1>
+    <navbar
+      :cart="cart"
+      :cartQty="cartQty"
+      :cartTotal="cartTotal"
+      @toggle="toggleSliderStatus"
+    ></navbar>
     <price-slider
       :sliderStatus="style.sliderStatus"
       :maximum.sync="maximum"
@@ -14,9 +20,9 @@
 </template>
 
 <script>
-// import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import ProductList from './components/ProductList.vue'
 import PriceSlider from './components/PriceSlider.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app',
@@ -26,14 +32,14 @@ export default {
       products: [],
       cart: [],
       style: {
-        sliderStatus: true,
+        sliderStatus: false,
       },
     }
   },
   components: {
-    // FontAwesomeIcon,
     ProductList,
     PriceSlider,
+    Navbar,
   },
   mounted: function () {
     fetch('https://hplussport.com/api/products/order/price')
@@ -42,7 +48,26 @@ export default {
         this.products = data
       })
   },
+  computed: {
+    cartTotal: function () {
+      let sum = 0
+      for (let key in this.cart) {
+        sum = sum + this.cart[key].product.price * this.cart[key].qty
+      }
+      return sum
+    },
+    cartQty: function () {
+      let qty = 0
+      for (let key in this.cart) {
+        qty = qty + this.cart[key].qty
+      }
+      return qty
+    },
+  },
   methods: {
+    toggleSliderStatus: function () {
+      this.style.sliderStatus = !this.style.sliderStatus
+    },
     addItem: function (product) {
       let productIndex
       let productExist = this.cart.filter(function (item, index) {
@@ -58,6 +83,13 @@ export default {
         this.cart[productIndex].qty++
       } else {
         this.cart.push({ product: product, qty: 1 })
+      }
+    },
+    deleteItem: function (id) {
+      if (this.cart[id].qty > 1) {
+        this.cart[id].qty--
+      } else {
+        this.cart.splice(id, 1)
       }
     },
   },
