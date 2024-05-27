4dea9245af12b97d55eb7eb565de53efc047327e
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-05-27 09:31:24 +0700

    Implementasi Modul di Vuejs

--- Chapter 6/vue-cli/src/App.vue
+++ Chapter 6/vue-cli/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <p class="animated fadeInRight">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+      magnam necessitatibus quod ipsam nemo labore a expedita, dolorem minima
+      repellendus? Iusto similique quas ex autem distinctio blanditiis odio
+      necessitatibus sunt!
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default {
-  name: "App",
+  name: 'app',
   components: {
-    HelloWorld,
+    FontAwesomeIcon,
   },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
